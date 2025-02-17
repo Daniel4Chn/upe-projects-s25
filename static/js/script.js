27e1abaa63104e534e3ac74a3a8354a64eb7948b
Koji Aquilino <kojiken@bu.edu>
2025-02-16 23:32:59 -0500

    Add comment to clarify files and functions

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,12 @@
+/*
+Test code for testing functionality for a button element. Can add extra below to experiment.
+*/
+
 let count = 0; // Starts counter at 0
 document.getElementById("Btn-inc").addEventListener("click", function() {
 	count++;
 	document.getElementById("counter").innerText = count; // Updates the text with each click/increment
 });
 
-console.log("JS file loaded");
+console.log("JS file loaded.");
 
