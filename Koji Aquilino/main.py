37edfeb476467d93dd4ed47de9f4297725d246eb
Koji Aquilino <kojiken@bu.edu>
2025-02-19 22:49:44 -0500

    Add instructions for properly running development server

--- Koji Aquilino/main.py
+++ Koji Aquilino/main.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+##########################
+# STARTUP INSTRUCTIONS:
+# Run Flask webserver with "flask run --host=0.0.0.0 --port=5001", and then navigate to http://<ip address>:5001
