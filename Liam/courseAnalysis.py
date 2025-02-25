2b9b80512e6701ea4cbfa2b40e4154975d2f068f
liammcdonald <liammcd3.14@gmail.com>
2025-02-24 22:12:01 -0500

    Created the functionality to fetch the data from the database and pull it into a course dictionary. Then created a method that uses the course dictionary to create a bipartite graph between the courses and the hub credits with an edge between a course and a hub credit if that course fulfills that credit

---
+++ Liam/courseAnalysis.py
@@ -0,0 +1,50 @@
+import sqlite3;
+from scrapenstore import my_classes;
+
+courseDict = {}
+graph = {}
+
+connection = sqlite3.connect("courses.db")
+cursor = connection.cursor()
+
+# res = connection.execute("SELECT * FROM COURSE")
+# row = res.fetchone()
+# print(row)
+
+# Copied from scrapenstore.py
+def initCourse(title, description):
+    course = {} # Create a dictionary to represent the course
+    for _, cat in my_classes:
+        course[cat] = 0 # Set all the hub categories to 0 indicating that the course does not fulfill that hub credit
+    course["Title"] = title # Set the title of the course to an empty string
+    course["Description"] = description # Set the description of the course to an empty string
+    return course
+
+def fetchData():
+    res = connection.execute("SELECT * FROM COURSE")
+    data = res.fetchall()
+
+    # Go row by row through the database and create a dictionary for each course and add it to the courseDict
+    for row in data:
+        courseDict[row[0]] = initCourse(row[1], row[2])
+        for i, cat in enumerate(my_classes):
+            courseDict[row[0]][cat] = row[i + 3]
+
+def createBipartiteGraph():
+    # Loop through each course in the courseDict
+    for course in courseDict:
+        graph[course] = {}
+        # Loop through each category in the course
+        for cat in my_classes:
+            # If the course fulfills the category, add an edge of value 1 to the graph
+            if courseDict[course][cat] == 1:
+                graph[course][cat] = 1
+
+def main():
+    fetchData()
+    createBipartiteGraph()
+    for course in graph:
+        print(course, graph[course])
+
+if __name__ == "__main__":
+    main()
