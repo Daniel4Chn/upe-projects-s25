b1a451c74932870e787d19bbffcb6d069153d6a9
Clemensssss <cli42@stuy.edu>
2025-03-03 17:31:55 -0500

    Added time variable, closest due date and get_stats() compound function

--- Clemens Li/analysis.py
+++ Clemens Li/analysis.py
@@ -1,5 +1,6 @@
 import gradescopeapi
 from gradescopeapi.classes.connection import GSConnection
+from datetime import datetime, timezone, timedelta
 
 raw_data = {}
 course_ids = []
@@ -11,7 +12,7 @@ def get_data(connection):
     global raw_data
     raw_data.clear()
     raw_data = connection.account.get_courses()
-    pass
+    return raw_data
 
 def categorize_data(connection):
     global course_data, course_ids, course_names, assignments
@@ -23,10 +24,40 @@ def categorize_data(connection):
     course_data = raw_data["student"]
     for id in course_ids:
         assignments.append(connection.account.get_assignments(id))
-        course_names.append(course_data[id].name) 
-    pass
-        
+        course_names.append(course_data[id].name)
+    return assignments
+
+def get_stats(course_assignments):
+    stats = [] # 0:average, 1:next_due_date
+    stats.append(average(course_assignments))
+    stats.append(next_due_date(course_assignments))
+    return stats
 
 # def analyze_courses(courses):
 
 # def analyze_assignments(assignments):
+
+def average(course_assignments):
+    points_earned = 0
+    points_possible = 0
+    average = []
+    for assignment in course_assignments:
+        if assignment.grade is not None:
+            points_earned += int(assignment.grade)
+            points_possible += int(assignment.max_grade)
+            average.append(points_earned/points_possible)
+    if len(average) == 0:
+        return "No grades found"
+    return points_earned / points_possible
+
+def next_due_date(course_assignments):
+    global stats
+    closest_date = course_assignments[len(course_assignments)-1].due_date
+    now = datetime.now(timezone(timedelta(hours=-5)))
+    print(now)
+    for assignment in course_assignments:
+        if assignment.submissions_status is not None and assignment.due_date > now:
+            if assignment.due_date < closest_date:
+                closest_date = assignment.due_date
+    return closest_date.strftime("%Y-%m-%d %H:%M:%S")
+
--- Clemens Li/main.py
+++ Clemens Li/main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import gradescopeapi
 from gradescopeapi.classes.connection import GSConnection
 import analysis as an
 
 email = ""
 password = ""
+file = ""
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable caching
+
+@app.after_request # Disable caching after every request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 @app.route("/")
 def index():
@@ -19,16 +28,26 @@ def receive_login():
     password = data.get("password")
     connection = GSConnection()
     connection.login(email, password)
-    an.get_data(connection)
-    an.categorize_data(connection)
+    temp_c = an.get_data(connection)
+    temp_a = an.categorize_data(connection)
+    file = open("temp.txt", "w")
+    file.write(str(temp_c) + "\n\n" + str(temp_a))
     return jsonify({"status": "success", "redirect": "/home", "message": (email, password)})
 
 @app.route("/home")
 def home_screen():
+    # Checking data validity
+    if not an.course_names or not an.assignments:
+        return redirect(url_for("index"))
     return render_template("home.html", course_names=an.course_names, assignments=an.assignments)
 
 @app.route("/course/<int:course_index>")
 def course_screen(course_index):
+    # Checking data validity
+    if not an.course_names or not an.assignments:
+            return redirect(url_for("index"))
+    
     course_name = an.course_names[course_index]
     course_assignments = an.assignments[course_index]
-    return render_template("course.html", course_name=course_name, course_assignments=course_assignments)
+    course_stats = an.get_stats(course_assignments)
+    return render_template("course.html", course_name=course_name, course_assignments=course_assignments, course_stats=course_stats)
